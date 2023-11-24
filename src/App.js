0ab4f3f81aef36be3fa999cd63406334f1882613
davidpacco <david.j.pacco@gmail.com>
2023-11-24 09:02:53 -0500

    Commit made from VS Code Azure Static Web Apps

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <>
+      <h1>Computer vision</h1>
+
+      <div>
+        <label htmlFor="imgUrl">Insert URL or type prompt: </label>
+      </div>
+      <div>
+        <input id="imgUrl" placeholder='Enter URL to analyze or textual prompt to generate an image' />
+      </div>
+      <div>
+        <button>Analyze</button>
+        <button>Generate</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
